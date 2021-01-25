b08dc06fd798066a99e9bda43c8ecafab89f90a0
Rudi Giesler <rudi@praekelt.org>
2021-01-25 17:05:56 +0200

    Remove psutil dependancy

--- vxwhatsapp/metrics.py
+++ vxwhatsapp/metrics.py
@@ -1,8 +1,6 @@
-import asyncio
 import time
 
-import psutil
-from prometheus_client import Counter, Gauge, Histogram
+from prometheus_client import Counter, Histogram
 from sanic import Sanic
 
 RQS_COUNT = Counter(
@@ -13,14 +11,6 @@ RQS_LATENCY = Histogram(
     "sanic_request_latency_sec",
     "Sanic Request Latency Histogram",
     ["method", "endpoint", "http_status"],
-)
-
-PROC_RSS_MEM_BYTES = Gauge(
-    "sanic_mem_rss_bytes", "Resident memory used by process running Sanic"
-)
-PROC_RSS_MEM_PERC = Gauge(
-    "sanic_mem_rss_perc",
-    "A per cent of total physical memory used by the process running Sanic",
 )
 
 
@@ -37,18 +27,3 @@ def setup_metrics_middleware(app: Sanic) -> None:
                 time.time() - request.ctx.start_time
             )
             RQS_COUNT.labels(request.method, request.path, response.status).inc()
-
-    async def periodic_memcollect_task(app: Sanic):
-        p = psutil.Process()
-        while True:
-            PROC_RSS_MEM_BYTES.set(p.memory_info().rss)
-            PROC_RSS_MEM_PERC.set(p.memory_percent())
-            await asyncio.sleep(30)
-
-    @app.listener("before_server_start")
-    async def start_memcollect_task(app, loop):
-        app.memcollect_task = loop.create_task(periodic_memcollect_task(app))
-
-    @app.listener("after_server_stop")
-    async def stop_memcollect_task(app, loop):
-        app.memcollect_task.cancel()
